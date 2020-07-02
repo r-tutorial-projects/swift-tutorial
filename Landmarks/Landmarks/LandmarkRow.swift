4a58affb91bdbae283be0bdbee246f20fa2bbdad
Ruben Vitt <dev@rubeen.dev>
2020-07-02 21:38:50 +0200

    :sparkles: solved chapter navigation
    https://developer.apple.com/tutorials/swiftui/building-lists-and-navigation

---
+++ Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ruben Vitt on 02.07.20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
