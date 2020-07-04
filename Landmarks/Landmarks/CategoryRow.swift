46577bd2f267348646da24b0ac01eac42e7ab86b
Ruben Vitt <dev@rubeen.dev>
2020-07-04 18:05:01 +0200

    :tada: init new tut
    https://developer.apple.com/tutorials/swiftui/working-with-ui-controls

--- Landmarks/Landmarks/CategoryRow.swift
+++ Landmarks/Landmarks/CategoryRow.swift
@@ -1,16 +1,16 @@
-//
-//  CategoryRow.swift
-//  Landmarks
-//
-//  Created by Ruben Vitt on 04.07.20.
-//  Copyright © 2020 Apple. All rights reserved.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing a scrollable list of landmarks.
+*/
 
 import SwiftUI
 
 struct CategoryRow: View {
     var categoryName: String
     var items: [Landmark]
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(self.categoryName)
@@ -21,7 +21,13 @@ struct CategoryRow: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(self.items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink(
+                            destination: LandmarkDetail(
+                                landmark: landmark
+                            )
+                        ) {
+                            CategoryItem(landmark: landmark)
+                        }
                     }
                 }
             }
@@ -30,25 +36,29 @@ struct CategoryRow: View {
     }
 }
 
-struct CategoryRow_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoryRow(categoryName: landmarkData[0].category.rawValue,
-                    items: Array(landmarkData.prefix(4)))
-    }
-}
-
-
 struct CategoryItem: View {
     var landmark: Landmark
     var body: some View {
         VStack(alignment: .leading) {
             landmark.image
+                .renderingMode(.original)
                 .resizable()
                 .frame(width: 155, height: 155)
                 .cornerRadius(5)
             Text(landmark.name)
+                .foregroundColor(.primary)
                 .font(.caption)
         }
         .padding(.leading, 15)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+        .environmentObject(UserData())
     }
 }
--- Landmarks/Landmarks/Home.swift
+++ Landmarks/Landmarks/Home.swift
@@ -1,22 +1,35 @@
-//
-//  Home.swift
-//  Landmarks
-//
-//  Created by Ruben Vitt on 04.07.20.
-//  Copyright © 2020 Apple. All rights reserved.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing featured landmarks above a list of all of the landmarks.
+*/
 
 import SwiftUI
 
 struct CategoryHome: View {
     var categories: [String: [Landmark]] {
-        Dictionary(grouping: landmarkData, by: {$0.category.rawValue})
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
     }
     
     var featured: [Landmark] {
-        landmarkData.filter {$0.isFeatured}
+        landmarkData.filter { $0.isFeatured }
     }
     
+    @State var showingProfile = false
+    
+    var profileButton: some View {
+        Button(action: { self.showingProfile.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("User Profile"))
+                .padding()
+        }
+    }
+
     var body: some View {
         NavigationView {
             List {
@@ -30,9 +43,16 @@ struct CategoryHome: View {
                     CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
                 .listRowInsets(EdgeInsets())
+                
+                NavigationLink(destination: LandmarkList()) {
+                    Text("See All")
+                }
             }
-            
-            .navigationBarTitle("Featured")
+            .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing: profileButton)
+            .sheet(isPresented: $showingProfile) {
+                Text("User Profile")
+            }
         }
     }
 }
@@ -47,5 +67,6 @@ struct FeaturedLandmarks: View {
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            .environmentObject(UserData())
     }
 }
