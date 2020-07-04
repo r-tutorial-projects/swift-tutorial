ca3a00e4e56ed8f92300f2d1199b6cf4175ee4f9
Ruben Vitt <dev@rubeen.dev>
2020-07-04 09:34:10 +0200

    :sparkles: work until section 5
    next one: https://developer.apple.com/tutorials/swiftui/composing-complex-interfaces#Add-Navigation-Between-Sections

---
+++ Landmarks/Landmarks/CategoryRow.swift
@@ -0,0 +1,54 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Ruben Vitt on 04.07.20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(categoryName: landmarkData[0].category.rawValue,
+                    items: Array(landmarkData.prefix(4)))
+    }
+}
+
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
---
+++ Landmarks/Landmarks/Home.swift
@@ -0,0 +1,51 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by Ruben Vitt on 04.07.20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var categories: [String: [Landmark]] {
+        Dictionary(grouping: landmarkData, by: {$0.category.rawValue})
+    }
+    
+    var featured: [Landmark] {
+        landmarkData.filter {$0.isFeatured}
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            
+            .navigationBarTitle("Featured")
+        }
+    }
+}
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image.resizable()
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
